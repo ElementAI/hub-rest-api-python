df35b0d63b4871fb3ca6268fe84c445bc9b23894
Guillaume Smaha <guillaume.smaha@gmail.com>
2019-03-07 14:53:18 -0500

    Add dry_run for delete_projects.py

--- examples/delete_project_eai.py
+++ examples/delete_project_eai.py
@@ -33,6 +33,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("project_name")
+parser.add_argument("run_delete")
 
 args = parser.parse_args()
 
@@ -40,10 +41,13 @@ hub = HubInstance()
 
 projects = hub.get_projects(parameters={'q':"name:{}".format(args.project_name)})
 
+from pprint import pprint
+
 if 'totalCount' in projects and projects['totalCount'] > 0:
 	for project in projects['items']:
 		if project['name'] != args.project_name:
 			continue
+		# pprint(project)
 
 		project_versions = hub.get_project_versions(
 			project
@@ -52,25 +56,29 @@ if 'totalCount' in projects and projects['totalCount'] > 0:
 		project_version_codelocations = None
 		if 'totalCount' in project_versions and project_versions['totalCount'] > 0:
 			for project_version in project_versions['items']:
+				# pprint(project_version)
 				project_version_codelocations = hub.get_version_codelocations(project_version)
 
 				if 'totalCount' in project_version_codelocations and project_version_codelocations['totalCount'] > 0:
 					code_location_urls = [c['_meta']['href'] for c in project_version_codelocations['items']]
 					for code_location_url in code_location_urls:
 						print("Deleting code location at: {}".format(code_location_url))
-						a = hub.execute_delete(code_location_url)
-						print(a)
+						if args.run_delete == "1":
+							a = hub.execute_delete(code_location_url)
+							print(a)
 				else:
 					print("Did not find any codelocations (scans) in version {} of project {}".format(project_version['versionName'], args.project_name))
 
 				print("Deleting project-version {} at: {}".format(project_version['versionName'], project_version['_meta']['href']))
-				a = hub.execute_delete(project_version['_meta']['href'])
-				print(a)
+				if args.run_delete == "1":
+					a = hub.execute_delete(project_version['_meta']['href'])
+					print(a)
 		else:
 			print("Did not find versions in project {}".format(args.project_name))
 		
 	print("Deleting project {} at: {}".format(project['name'], project['_meta']['href']))
-	a = hub.execute_delete(project['_meta']['href'])
-	print(a)
+	if args.run_delete == "1":
+		a = hub.execute_delete(project['_meta']['href'])
+		print(a)
 else:
 	print("Did not find project with name {}".format(args.project_name))
